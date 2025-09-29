b799761025ef250885e2dc09df4ea55fdfe0de08
CalumDryburgh-SL <cdryburgh@scottlogic.com>
2025-09-29 15:49:48 +0100

    adding book to collction using new calum-user, compares response to verify successful deletion and addition.

--- tests/api/requests/delete-books-collection.ts
+++ tests/api/requests/delete-books-collection.ts
@@ -1,21 +1,40 @@
-import { APIRequestContext } from '@playwright/test';
-import { buildUrl } from '../../utils/apiUrlBuilder';
-import { executeRequest } from '../../utils/apiRequestUtils';
-import endpoints from '../../utils/apiEndpoints';
-import methods from '../../utils/apiMethods';
+import { APIRequestContext } from "@playwright/test";
+import { buildUrl } from "../../utils/apiUrlBuilder";
+import { executeRequest } from "../../utils/apiRequestUtils";
+import endpoints from "../../utils/apiEndpoints";
+import methods from "../../utils/apiMethods";
 
-async function deleteAllBooksByUser(apiContext: APIRequestContext, userId: string) {
+async function deleteAllBooksByUser(
+  apiContext: APIRequestContext,
+  userId: string
+) {
   const method = methods.delete;
   const requestOptions = {};
   const requestUrl = buildUrl(endpoints.books.delete, userId);
-  const response = await executeRequest(apiContext, requestUrl, method, requestOptions);
+  const response = await executeRequest(
+    apiContext,
+    requestUrl,
+    method,
+    requestOptions
+  );
+  return response;
 }
 
-async function deleteBookAPIByIsbn(apiContext: APIRequestContext, userId: string, isbn: string) {
+async function deleteBookAPIByIsbn(
+  apiContext: APIRequestContext,
+  userId: string,
+  isbn: string
+) {
   const method = methods.delete;
-  const requestOptions = { data: { isbn: isbn, userId: userId }};
-  const requestUrl = buildUrl(endpoints.books.delete);
-  const response = await executeRequest(apiContext, requestUrl, method, requestOptions);
+  const requestOptions = { data: { isbn: isbn, userId: userId } };
+  const requestUrl = buildUrl(endpoints.books.delete, userId);
+  const response = await executeRequest(
+    apiContext,
+    requestUrl,
+    method,
+    requestOptions
+  );
+  return response;
 }
 
 export default { deleteAllBooksByUser, deleteBookAPIByIsbn };
